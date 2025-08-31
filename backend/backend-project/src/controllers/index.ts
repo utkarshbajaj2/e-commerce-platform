153698b77befeeaa471c292fca6207216cafde00
Utkarsh Bajaj <utkarshbajaj2@gmail.com>
2025-08-31 20:33:21 +0530

    Delete backend/backend-project directory

--- backend/backend-project/src/controllers/index.ts
+++
@@ -1,9 +0,0 @@
-class IndexController {
-    getIndex(req, res) {
-        res.send('Welcome to the API!');
-    }
-
-    // Additional methods can be added here for handling other routes
-}
-
-export default IndexController;
